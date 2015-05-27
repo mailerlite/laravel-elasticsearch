d096c50a5f4957150f38613feb2908ee2b9fa87a
Colin Viebrock <c.viebrock@tippingcanoe.com>
2015-05-27 13:52:21 -0500

    update config to work with 2.0 client and readme updates

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -1,7 +1,8 @@
 <?php namespace Cviebrock\LaravelElasticsearch;
 
-use Elasticsearch\Client;
+use Elasticsearch\ClientBuilder;
 use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+use Psr\Log\LoggerInterface;
 
 
 class ServiceProvider extends BaseServiceProvider {
@@ -45,7 +46,79 @@ class ServiceProvider extends BaseServiceProvider {
 
 			$config = $app['config']['elasticsearch'] ?: $app['config']['elasticsearch::config'];
 
-			return new Client($config);
+			$clientBuilder = ClientBuilder::create();
+
+			// Configure hosts
+
+			$clientBuilder->setHosts($config['hosts']);
+
+			// Configure SSL
+
+			if ($config['sslVerification'] !== null) {
+				$clientBuilder->setSSLVerification($config['sslVerification']);
+			}
+
+			// Configure logging
+
+			if ($config['logging']) {
+				if ($config['logObject'] instanceof LoggerInterface) {
+					$clientBuilder->setLogger($config['logObject']);
+				} else {
+					$path = $config['logPath'];
+					$level = $config['logLevel'];
+					$logger = ClientBuilder::defaultLogger($path, $level);
+					$clientBuilder->setLogger($logger);
+				}
+			}
+
+			// Configure retries
+
+			if ($config['retries'] !== null) {
+				$clientBuilder->setRetries($config['retries']);
+			}
+
+
+			// Configure HTTP Handler
+
+			if ($config['httpHandler'] !== null) {
+				$clientBuilder->setHandler($config['httpHandler']);
+			}
+
+			// Configure Connection Pool
+
+			if ($config['connectionPool'] !== null) {
+				$clientBuilder->setConnectionPool($config['connectionPool']);
+			}
+
+			// Configure Connection Selector
+
+			if ($config['connectionSelector'] !== null) {
+				$clientBuilder->setSelector($config['connectionSelector']);
+			}
+
+			// Configure Serializer
+
+			if ($config['serializer'] !== null) {
+				$clientBuilder->setSerializer($config['serializer']);
+			}
+
+			// Configure Connection Factory
+
+			if ($config['connectionFactory'] !== null) {
+				$clientBuilder->setConnectionFactory($config['connectionFactory']);
+			}
+
+			// Configure Endpoint
+
+			if ($config['endpoint'] !== null) {
+				$clientBuilder->setEndpoint($config['endpoint']);
+			}
+
+
+			// Return the client
+
+			return $clientBuilder->build();
+
 		});
 	}
 
--- src/config/config.php
+++ src/config/config.php
@@ -1,37 +1,139 @@
 <?php
 
-/**
- * There are several options that you can use to customize the
- * Elasticsearch client, but these are main ones.
- *
- * If you want details on the other configuration options, they
- * are documented on the main ES website:
- *
- * https://www.elastic.co/guide/en/elasticsearch/client/php-api/current/_configuration.html
- */
-
-
 return [
 
 	/**
-	 * Array of hosts that Elasticsearch will connect to.
+	 * Hosts
+	 *
+	 * This is an array of hosts that the client will connect to. It can be a
+	 * single host name, or an array if you are running a cluster of
+	 * Elasticsearch instances.
+	 *
+	 * This is the only configuration value that is mandatory.
+	 *
+	 * @see https://www.elastic.co/guide/en/elasticsearch/client/php-api/2.0/_configuration.html#_host_configuration
 	 */
 
 	'hosts' => [
 		'localhost:9200'
 	],
 
+
 	/**
-	 * If you want to enable and/or customize the logging, update the following
-	 * settings.
+	 * SSL
+	 *
+	 * If your ES instance uses an out-dated or self-signed SSL certificate,
+	 * you will need to pass in the certificate bundle.  This can either be the path
+	 * to the certificate file (for self-signed certs), or a package like
+	 * https://github.com/Kdyby/CurlCaBundle.  See the documentation below for all the details.
+	 *
+	 * If you are using SSL instances, and the certificates are up-to-date and signed
+	 * by a public certificate authority, then you can leave this null and just use "https"
+	 * in the host path(s) above and you should be fine.
+	 *
+	 * @see https://www.elastic.co/guide/en/elasticsearch/client/php-api/2.0/_security.html#_ssl_encryption_2
+	 */
+
+	'sslVerification' => null,
+
+
+	/**
+	 * Logging
+	 *
+	 * Logging is handled by passing in an instance of Monolog\Logger (which is
+	 * what Laravel's logger is by default).
+	 *
+	 * If logging is enabled, you either need to set the path and log level (some
+	 * defaults are given for you below), or you can use a custom logger by
+	 * setting 'logObject' to an instance of Psr\Log\LoggerInterface.
+	 *
+	 * (Note: logObject takes precedent over logPath/logLevel, so leave logObject
+	 * null if you just want regular old logging.)
+	 *
+	 * @see https://www.elastic.co/guide/en/elasticsearch/client/php-api/2.0/_configuration.html#enabling_logger
 	 */
 
 	'logging' => false,
 
-	'logObject' => null,
-
 	'logPath' => storage_path('/logs/elasticsearch.log'),
 
-	'logLevel' => Psr\Log\LogLevel::WARNING,
+	'logLevel' => Monolog\Logger::INFO,
+
+	'logObject' => null,
+
+
+	/**
+	 * Retries
+	 *
+	 * By default, the client will retry n times, where n = number of nodes in your cluster.
+	 * If you would like to disable retries, or change the number, you can do so here.
+	 *
+	 * @see https://www.elastic.co/guide/en/elasticsearch/client/php-api/2.0/_configuration.html#_set_retries
+	 */
+
+	'retries' => null,
+
+
+	/**
+	 * The remainder of the configuration options can almost always be left as-is unless you have
+	 * specific reasons to change them.  Refer to the appropriate sections in the Elasticsearch
+	 * documentation for what each option does and what values it expects.
+	 */
+
+	/**
+	 * HTTP Handler
+	 *
+	 * @see https://www.elastic.co/guide/en/elasticsearch/client/php-api/2.0/_configuration.html#_configure_the_http_handler
+	 * @see http://ringphp.readthedocs.org/en/latest/client_handlers.html
+	 */
+
+	'httpHandler' => null,
+
+
+	/**
+	 * Connection Pool
+	 *
+	 * @see https://www.elastic.co/guide/en/elasticsearch/client/php-api/2.0/_configuration.html#_setting_the_connection_pool
+	 * @see https://www.elastic.co/guide/en/elasticsearch/client/php-api/2.0/_connection_pool.html
+	 */
+
+	'connectionPool' => null,
+
+
+	/**
+	 * Connection Selector
+	 *
+	 * @see https://www.elastic.co/guide/en/elasticsearch/client/php-api/2.0/_configuration.html#_setting_the_connection_selector
+	 * @see https://www.elastic.co/guide/en/elasticsearch/client/php-api/2.0/_selectors.html
+	 */
+
+	'connectionSelector' => null,
+
+
+	/**
+	 * Serializer
+	 *
+	 * @see https://www.elastic.co/guide/en/elasticsearch/client/php-api/2.0/_configuration.html#_setting_the_serializer
+	 * @see https://www.elastic.co/guide/en/elasticsearch/client/php-api/2.0/_serializers.html
+	 */
+
+	'serializer' => null,
+
+
+	/**
+	 * Connection Factory
+	 *
+	 * @see https://www.elastic.co/guide/en/elasticsearch/client/php-api/2.0/_configuration.html#_setting_a_custom_connectionfactory
+	 */
+
+	'connectionFactory' => null,
+
+	/**
+	 * Endpoint
+	 *
+	 * @see https://www.elastic.co/guide/en/elasticsearch/client/php-api/2.0/_configuration.html#_set_the_endpoint_closure
+	 */
+
+	'endpoint' => null,
 
 ];
