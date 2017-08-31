90602e89c1aaaada0261f566085c7d1ca62ac41d
Colin Viebrock <c.viebrock@tippingcanoe.com>
2017-08-31 15:27:02 -0500

    better tests?

--- tests/ServiceProviderTests.php
+++ tests/ServiceProviderTests.php
@@ -2,7 +2,7 @@
 
 use Cviebrock\LaravelElasticsearch\Factory;
 use Cviebrock\LaravelElasticsearch\Manager;
-use Elasticsearch as ElasticsearchFacade;
+use Elasticsearch;
 use Elasticsearch\Client;
 
 
@@ -21,11 +21,23 @@ class ServiceProviderTests extends TestCase
         $this->assertInstanceOf(Client::class, $client);
     }
 
+    /**
+     * Test that the facade works.
+     * @todo This seems a bit simplistic ... maybe a better way to do this?
+     */
     public function testFacadeWorks() {
-        $info = ElasticsearchFacade::info();
+        $ping = Elasticsearch::ping();
 
-        var_dump($info);
-        $this->assertTrue(is_array($info));
+        $this->assertTrue($ping);
     }
 
+    /**
+     * Test we can get the ES info.
+     */
+    public function testInfoWorks() {
+        $info = Elasticsearch::info();
+
+        $this->assertTrue(is_array($info));
+        $this->assertArrayHasKey('cluster_name', $info);
+    }
 }
