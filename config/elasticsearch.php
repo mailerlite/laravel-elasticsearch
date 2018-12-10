9dce45edf6045a8b7c7741f3ae1bc34926fbf1ce
Matrix86 <matrix86@tuxmealux.net>
2018-12-10 17:08:30 +0100

    Fix #43: added support to AWS Elastic Search

--- config/elasticsearch.php
+++ config/elasticsearch.php
@@ -41,11 +41,15 @@ return [
 
             'hosts' => [
                 [
-                    'host'   => env('ELASTICSEARCH_HOST', 'localhost'),
-                    'port'   => env('ELASTICSEARCH_PORT', 9200),
-                    'scheme' => env('ELASTICSEARCH_SCHEME', null),
-                    'user'   => env('ELASTICSEARCH_USER', null),
-                    'pass'   => env('ELASTICSEARCH_PASS', null),
+                    'host'       => env('ELASTICSEARCH_HOST', 'localhost'),
+                    'port'       => env('ELASTICSEARCH_PORT', 9200),
+                    'scheme'     => env('ELASTICSEARCH_SCHEME', null),
+                    'user'       => env('ELASTICSEARCH_USER', null),
+                    'pass'       => env('ELASTICSEARCH_PASS', null),
+                    'aws'        => env('AWS_ELASTICSEARCH_ENABLED', false),
+                    'aws_region' => env('AWS_REGION', ''),
+                    'aws_key'    => env('AWS_ACCESS_KEY_ID', ''),
+                    'aws_secret' => env('AWS_SECRET_ACCESS_KEY', '')
                 ],
             ],
 
