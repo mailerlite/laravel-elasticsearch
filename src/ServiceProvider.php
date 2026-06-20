7eb508f45fbb19fd313906113b2c4a86df045a80
Shuvro Roy <shuvro.nsu.cse@gmail.com>
2026-06-20 13:49:56 +0600

    chore: remove lumen support

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -13,7 +13,6 @@ use Elastic\Elasticsearch\Client;
 use Illuminate\Container\Container;
 use Illuminate\Foundation\Application as LaravelApplication;
 use Illuminate\Support\ServiceProvider as BaseServiceProvider;
-use Laravel\Lumen\Application as LumenApplication;
 
 /**
  * Class ServiceProvider
@@ -61,8 +60,6 @@ class ServiceProvider extends BaseServiceProvider
 
         if ($this->app instanceof LaravelApplication) {
             $this->publishes([$source => config_path('elasticsearch.php')], 'config');
-        } elseif ($this->app instanceof LumenApplication) {
-            $this->app->configure('elasticsearch');
         }
 
         $this->mergeConfigFrom($source, 'elasticsearch');
